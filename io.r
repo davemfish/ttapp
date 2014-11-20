99d7b13a767d7d7673cb9f1d1d09b7108ffb067f
davemfish <davemfish@gmail.com>
2014-11-20 15:25:24 -0800

    general symbology rules for any additional layer; quick fix for single category layers

--- io.r
+++ io.r
@@ -28,7 +28,7 @@ Cut2Num <- function(x){
 ## write geojson data
 LoadSpace <- function(ws, outpath){
   #ws <- "C:/Users/dfisher5/Documents/Shiny/CoastalVulnerability/data/Florida_CV_inputs_WGS84/Florida_CV_inputs_WGS84/CV_out_200m"
-  ce <- read.table(file.path(ws, "coastal_exposure.csv"), sep=",", colClasses="numeric", header=T)
+  ce <- read.table(file.path(ws, "coastal_exposure.csv"), sep=",", colClasses="numeric", header=T, check.names=F)
   aoi <- raster(file.path(ws, "00_PRE_aoi.tif"))
   points.wgs84 <- rgdal::project(as.matrix(ce[,1:2]), proj=projection(aoi), inv=T)
   
@@ -47,17 +47,22 @@ LoadSpace <- function(ws, outpath){
       ids <- ids[order(ids)]
       #ids <- c(ids)
       #legbrks <- legbrks[ids]
-      
-      leglabs <- list()
-      for (i in 2:length(legbrks)){
-#         if (i == 1) { 
-#           leglabs[[i]] <- legbrks[i] 
-#         } else {
+      if (length(ids) > 1){
+        leglabs <- list()
+        for (i in 2:length(legbrks)){
+          #         if (i == 1) { 
+          #           leglabs[[i]] <- legbrks[i] 
+          #         } else {
           leglabs[[i-1]] <- paste(legbrks[i-1], "-", legbrks[i])
-#        }
+          #        }
+        }
+        legcols <- brewer.pal(4, "YlOrRd")[ids]
+        leg.list[[j]] <- list(layer=nm, leglabs=unlist(leglabs), legcols=legcols)
+      } else {
+        legcols <- c(brewer.pal(4, "YlOrRd")[ids], NA)
+        leglabs <- c(as.character(legbrks)[ids], NA)
+        leg.list[[j]] <- list(layer=nm, leglabs=unlist(leglabs), legcols=legcols)
       }
-      legcols <- brewer.pal(4, "YlOrRd")[ids]
-    leg.list[[j]] <- list(layer=nm, leglabs=unlist(leglabs), legcols=legcols)
       
     }
     if (nm %in% c("shore_exposure", "erodible_shoreline")){
@@ -96,6 +101,38 @@ LoadSpace <- function(ws, outpath){
       legcols <- c("#d3d3d3", brewer.pal(4, "Purples")[ids])
       leg.list[[j]] <- list(layer=nm, leglabs=unlist(leglabs), legcols=legcols)
     }
+    if (!(nm %in% c("habitat_role", "shore_exposure", "erodible_shoreline", "coastal_exposure", "geomorphology", "natural_habitats", "wave_exposure", "surge_potential", "coastal_exposure_no_habitats", "sea_level_rise", "relief"))){
+      brks <- quantile(ce[,nm], seq(0,1,.25))
+      brks <- unique(brks)
+      if (length(brks) > 1){
+        cats <- cut(ce[,nm], breaks=brks, right=F, include.lowest=T)
+        cols <- brewer.pal(length(unique(brks)), "PuRd")[as.numeric(cats)]
+        brks.list <- Cut2Num(cats)
+        num.brks <- brks.list[["brks"]]
+        legbrks <- round(num.brks, digits=3)
+        #legbrks[1] <- 0
+        ids <- brks.list[["ids"]]
+        ids <- ids[order(ids)]
+        #ids <- c(ids)
+        #legbrks <- legbrks[ids]
+        
+        leglabs <- list()
+        for (i in 2:length(legbrks)){
+          #         if (i == 1) { 
+          #           leglabs[[i]] <- legbrks[i] 
+          #         } else {
+          leglabs[[i-1]] <- paste(legbrks[i-1], "-", legbrks[i])
+          #        }
+        }
+        legcols <- brewer.pal(4, "PuRd")[ids]
+        leg.list[[j]] <- list(layer=nm, leglabs=unlist(leglabs), legcols=legcols)
+      } else {
+        leglabs <- c(as.character(brks), NA)
+        cols <- "#8c2d04"
+        legcols <- c(cols, NA)
+        leg.list[[j]] <- list(layer=nm, leglabs=leglabs, legcols=legcols)
+      }
+    }
     cols <- sub(cols, pattern="#", replacement="hex")
     df <- cbind(data.frame(ce[,nm]), data.frame(cols))
     names(df)[1] <- c(nm)
@@ -104,14 +141,14 @@ LoadSpace <- function(ws, outpath){
     if(!(paste(nm, ".geojson", sep="") %in% jsonfiles)){
       writeOGR(obj=spdf, dsn=paste(outpath, nm, ".geojson", sep=""), layer="layer", driver="GeoJSON", overwrite=T)
     }
-  writeLines(toJSON(leg.list[5:length(leg.list)]), file.path(outpath, "legend.json"))
-  }
+}
+    writeLines(toJSON(leg.list[5:length(leg.list)]), file.path(outpath, "legend.json"))
 }
 
 ## doit
-workspace <- paste("/var/www/html/ttapp/tmp/", sess, "/", sep='')
-outspace <- paste("/var/www/html/ttapp/tmp/", sess, "/", sep='')
-#workspace <- "C:/Users/dfisher5/Documents/Shiny/CoastalVulnerability/data/Florida_CV_inputs_WGS84/Florida_CV_inputs_WGS84/CV_out_200m"
-#outspace <- "C:/Users/dfisher5/Documents/Shiny/www/ttapp/tmp/e2vji6s7i8doeibat2h74md1l2/"
+#workspace <- paste("/var/www/html/ttapp/tmp/", sess, "/", sep='')
+#outspace <- paste("/var/www/html/ttapp/tmp/", sess, "/", sep='')
+workspace <- "C:/Users/dfisher5/Documents/Shiny/CoastalVulnerability/data/CV"
+outspace <- "C:/Users/dfisher5/Documents/Shiny/www/ttapp/tmp/9dhb5qmmjoku93biblcvilfit2/"
 LoadSpace(workspace, outspace)
 
